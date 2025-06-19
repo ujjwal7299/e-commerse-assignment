d7f29e92861ed49634cf21b99ed48f95b34dc1a8
Ujjwal Kumar Singh <142611782+ujjwal7299@users.noreply.github.com>
2025-06-19 20:45:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect('mongodb+srv://test-ujjwal:Ujjwal12345@cluster0.hhnkp.mongodb.net/test')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 module.exports = mongoose;
