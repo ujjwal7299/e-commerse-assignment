c9c6a2a883fe382b020143a74918a510e2d1243c
Ujjwal Kumar Singh <142611782+ujjwal7299@users.noreply.github.com>
2025-06-19 20:41:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/logout', (req, res) => {
 //   console.log(' Server running at http://localhost:4000');
 //   import('open').then(open => open.default('http://localhost:4000'));
 // });
-const PORT = process.env.PORT || 4000;
+const PORT =  4000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
